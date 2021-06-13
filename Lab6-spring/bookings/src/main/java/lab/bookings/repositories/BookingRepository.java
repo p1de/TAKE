4cf5e9fd7ece44e7966634107241d6513dee42ac
Szymon Pawlak <szymekpaw5@gmail.com>
2021-06-13 10:42:29 +0200

    folder name changes

--- Lab6-spring/bookings/src/main/java/lab/bookings/repositories/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package lab.bookings.repositories;
-
-import lab.bookings.models.Booking;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookingRepository extends CrudRepository<Booking, Long> {
-}
